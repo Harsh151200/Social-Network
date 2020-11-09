21cbcebcdddf98d257a6ef1a4cf0e0037bddb5dc
Tanuj-45 <tanujpatel46@gmail.com>
2020-11-09 21:47:55 +0530

    4th video done

--- functions/functions.php
+++ functions/functions.php
@@ -198,4 +198,45 @@ function get_posts(){
 	include("pagination.php");
 }
 
+    function single_post(){
+		if (isset($_GET['post_id'])){
+			global $con;
+
+			$get_id = $_GET['post_id'];
+
+			$get_posts = "select * from posts where post_id='$get_id'";
+
+			$run_posts = mysqli_query($con,$get_posts);
+
+			$row_posts = mysqli_fetch_array($run_posts);
+
+			$post_id = $row_posts['post_id'];
+			$user_id = $row_posts['user_id'];
+			$content = $row_posts['post_content'];
+			$upload_image = $row_posts['upload_image'];
+			$post_date = $row_posts['post_date'];
+
+			$user = "select * from users where user_id='$user_id' AND posts='yes'";
+
+			$run_user = mysqli_query($con, $user);
+
+			$row_user = mysqli_fetch_array($run_user);
+
+			$user_name = $row_user['user_name'];
+			$user_image = $row_user['user_image'];
+
+			$user_com = $_SESSION['user_email'];
+			$get_com = "select * from the users where user_email='$user_email'";
+			$run_com = mysqli_query($con, $get_com);
+			$row_com = mysqli_fetch_array($run_com);
+
+			$user_com_id = $row_com['user_id'];
+			$user_com_name = $row_com['user_name'];
+
+
+
+
+			}
+
+	}
 ?>
